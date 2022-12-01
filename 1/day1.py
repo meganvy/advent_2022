243bf25562700721848a344149939df5a7c3dba8
domega <domega@twitch.tv>
2022-12-01 00:16:08 -0800

    day 1

---
+++ 1/day1.py
@@ -0,0 +1,35 @@
+import os
+
+def part_one():
+    max_calories = 0
+    script_dir = os.path.dirname(__file__)
+    with open(os.path.join(script_dir, "input.txt"), "r") as f:
+        val = 0
+        for line in f:
+            if line != "\n":
+                val += int(line)
+            else:
+                max_calories = max(max_calories, val)
+                val = 0
+    return max_calories
+
+
+def part_two():
+    elves = {}
+    script_dir = os.path.dirname(__file__)
+    with open(os.path.join(script_dir, "input.txt"), "r") as f:
+        carrying = 0
+        for line in f:
+            if line != "\n":
+                carrying += int(line)
+            else:
+                elves[carrying] = 0
+                carrying = 0
+    elves[carrying] = 0
+    count = 0
+    top_three = sorted(elves.keys())[-3:]
+    return sum(top_three)
+
+
+print(part_one())
+print(part_two())
